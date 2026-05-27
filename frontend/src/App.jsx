cc30be32f540f879be7c4ee29455326b7d945e4f
JORCE23 <jorgeceballossegura@gmail.com>
2026-05-27 02:36:50 -0400

    pagina de ventas + conexion app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import { Sidebar } from './components/layout/Sidebar';
 import { Login } from './pages/Login';
